a1797a4ee55579bd1c05b62ade054b5771ae24b8
Ciro <cirobsb_br@yahoo.com.br>
2025-08-08 12:12:40 -0300

    php upgrade

--- back/quality-gate.sh
+++ back/quality-gate.sh
@@ -1,2 +1,6 @@
+#fix
+vendor/bin/rector
+vendor/bin/phpcbf src core
+#validate
 php ./vendor/bin/phpmd . text phpmd.xml --exclude 'tests/*,vendor/*,var/*,migrations/*'
 vendor/bin/phpstan analyse -l 10 src core
