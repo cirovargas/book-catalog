00084077b98b40b60dc1eec61bf0727525a04e4a
Ciro <cirobsb_br@yahoo.com.br>
2025-11-14 12:28:06 -0300

    Environment

--- back/migrations/Version20250807170944.php
+++ back/migrations/Version20250807170944.php
@@ -22,7 +22,7 @@ final class Version20250807170944 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE app_user (id SERIAL NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_88BDF3E9E7927C74 ON app_user (email)');
-        $this->addSql("INSERT INTO app_user (id, email, roles, password) VALUES (DEFAULT, 'admin@project.com', '[\"ROLE_USER\"]', '$2y$13\$wlto/IFebs/61rUp9GiB8u/hGFApUhsXGzz8QqgsC8scqnHfuzkri')");
+        $this->addSql("INSERT INTO app_user (id, email, roles, password) VALUES (DEFAULT, 'admin@project.com', '[\"ROLE_USER\",\"ROLE_ADMIN\"]', '$2y$13\$wlto/IFebs/61rUp9GiB8u/hGFApUhsXGzz8QqgsC8scqnHfuzkri')");
     }
 
     public function down(Schema $schema): void
